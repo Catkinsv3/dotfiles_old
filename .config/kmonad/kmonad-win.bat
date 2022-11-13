949be2b856cd8754d873196d873f210f3ad75e2b
Chris <catkinsv3@gmail.com>
2022-11-13 22:38:15 +1000

    support windows

--- .config/kmonad/kmonad-win.bat
+++ .config/kmonad/kmonad-win.bat
@@ -1,1 +1,8 @@
+:: Starts the kmonad binary with the .kbd configuration as the arg.
+:: 	(find the exe binary here: https://github.com/kmonad/kmonad/releases)
+:: Update to your local binary and .kbd path.
+:: Terminal is hidden after starting.
+:: Don't forget to add this .bat to your antivirus exceptions, as it might detect it as a false positive.
+:: Put a shortcut to this in shell:startup.
+
 powershell "start C:\Users\catki\Documents\dotfiles\.config\kmonad\kmonad-win.exe -Args \"C:\Users\catki\Documents\dotfiles\.config\kmonad\colemakdh_extended_windows.kbd" -WindowStyle Hidden"
