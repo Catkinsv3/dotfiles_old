712663567e4a66b6927eeada094db4f33d2999fa
Chris <catkinsv3@gmail.com>
2022-11-13 22:33:31 +1000

    support windows

---
+++ .config/kmonad/kmonad-win.bat
@@ -0,0 +1,1 @@
+powershell "start C:\Users\catki\Documents\dotfiles\.config\kmonad\kmonad-win.exe -Args \"C:\Users\catki\Documents\dotfiles\.config\kmonad\colemakdh_extended_windows.kbd" -WindowStyle Hidden"
