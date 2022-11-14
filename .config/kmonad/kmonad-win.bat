dd858c9240a880c2d3b9ffa6f97d07d5312cd635
catkinsv3 <catkinsv3@gmail.com>
2022-11-14 23:28:21 +1000

    add mac support

--- .config/kmonad/kmonad-win.bat
+++
@@ -1,9 +0,0 @@
-:: Starts the kmonad binary with the .kbd configuration as the arg.
-:: 	(find the exe binary here: https://github.com/kmonad/kmonad/releases)
-:: Update to your local binary and .kbd path.
-:: Terminal is hidden after starting.
-:: Don't forget to add this .bat to your antivirus exceptions, as it might detect it as a false positive.
-:: Put a shortcut to this in shell:startup.
-::   https://github.com/kmonad/kmonad/blob/master/doc/faq.md#making-a-launcher
-
-powershell "start C:\Users\catki\Documents\dotfiles\.config\kmonad\kmonad-win.exe -Args \"C:\Users\catki\Documents\dotfiles\.config\kmonad\colemakdh_extended_windows.kbd" -WindowStyle Hidden"
